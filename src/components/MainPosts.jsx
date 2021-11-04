d0b8f4a94f9133ce7f3b1ebe2fc9f88812cd1ad3
vladp <de5pot162@gmail.com>
2021-11-04 14:01:04 +0200

    done scroll

--- src/components/MainPosts.jsx
+++ src/components/MainPosts.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Post from "./Post";
 
 function MainPosts({posts}) {
+  const [newPosts, setNewPosts] = useState([]);
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler);
+    return function () {
+      document.removeEventListener('scroll', scrollHandler);
+    }
+  }, [newPosts]);
+
+  const scrollHandler = (e) => {
+    if(e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100) {
+      return setNewPosts([...newPosts, ...posts]);
+    }
+  }
+
   return (
     <div className="interesting-pots">
       <p className="block-header">Interesting</p>
 
       <div className="interesting-posts__body">
-        {posts.map((item, i) => (
+        {posts.map(item => (
           <Post
             key={item.id}
             image={item.image}
@@ -16,6 +32,17 @@ function MainPosts({posts}) {
             text={item.text}
           />
         ))}
+        <div className="scroll-items">
+          {/*{newPosts.map(item => (*/}
+          {/*  <Post*/}
+          {/*    key={uuidv4()}*/}
+          {/*    image={item.image}*/}
+          {/*    date={item.date}*/}
+          {/*    header={item.header}*/}
+          {/*    text={item.text}*/}
+          {/*  />*/}
+          {/*))}*/}
+        </div>
       </div>
     </div>
   );
